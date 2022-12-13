7886bff8e0d018b5d75b903e0a2426f4125e5f80
Sungho95 <tjdgh1785@gmail.com>
2022-12-14 01:17:57 +0900

    Initial commit

---
+++ src/main/java/com/sungho/traveltogether/TraveltogetherApplication.java
@@ -0,0 +1,13 @@
+package com.sungho.traveltogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveltogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveltogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungho/traveltogether/TraveltogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungho.traveltogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveltogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
